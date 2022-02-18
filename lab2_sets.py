2c01fcf622d781d4ac4989767eb6fed6ee9859cf
asskicker54 <jnreey547@gmail.com>
2022-02-18 13:13:57 +0700

    lab2_sets

---
+++ lab2_sets.py
@@ -0,0 +1,42 @@
+import random
+def task1():
+    a = [random.randint(0,10) for i in range(10)]
+    #print(a)
+    print(len(set(a)))
+
+def task2():
+    a = [random.randint(0,10) for i in range(10)]
+    m = [random.randint(0,10) for i in range(10)]
+    #print(a)
+    #print(m)
+
+    print(len(set(a) & set(m)))
+
+def task3():
+    a = [random.randint(0,10) for i in range(10)]
+    m = [random.randint(0,10) for i in range(10)]
+    #print(a)
+    #print(m)
+
+    print(sorted(list(set(a) & set(m))))
+
+def task4():
+    line = input()
+    arr = line.split(" ")
+    repeats = set()
+
+    for i in arr:
+        if i in repeats:
+            print("yes")
+        else:
+            repeats.add(i)
+            print('no')
+
+def task5():
+    words = set()
+    lines = int(input())
+    for i in range(lines):
+        words.update(input().split())
+    print(len(words))
+
+task5()
